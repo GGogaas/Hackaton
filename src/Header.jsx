97a62d6d94d595b7771f3313c1d7d8225eaa62f3
JacobCod3r <jak.mar.sp7@gmail.com>
2024-04-16 10:41:08 +0200

    Burger menu icon and background
    Burger menu icon, positioned, will change color to white, applied background but it need fixes with join when repeat is on. New image to WarGaming and new clicking style to cards. Changed structure of menus (burger one too).

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,14 +3,14 @@ import image from './assets/logo1-removebg-preview-smaller.png'
 function Header(){
     
     return (
-        <div className="header">
+        <>
             <div className="logo"><a href="index.html"><img className="logo_zdj" src={image} /></a></div>
     
             <div className="interactive">
                 <div className="info"><a href="info.html"><button>Contact</button></a></div>
                 <div className="info"><a href="info.html"><button>Informations</button></a></div>
             </div>
-        </div>
+        </>
     )
 
 }
--- src/HeaderBurger.jsx
+++ src/HeaderBurger.jsx
@@ -1,17 +1,18 @@
 import image from './assets/logo1-removebg-preview-smaller.png'
+import burger from './assets/burger.png'
 
 function HeaderBurger(){
     
     return (
-        <div className="header">
+        <>
             <div className="logo"><a href="index.html"><img className="logo_zdj" src={image} /></a></div>
     
             <div className="interactive">
                 <div className="info"><a href="info.html"><button>Contact</button></a></div>
                 <div className="info"><a href="info.html"><button>Informations</button></a></div>
-                <div className="burger"><button><img></img></button></div>
+                <div className="burger"><button><img className="burgerIMG" src={burger}></img></button></div>
             </div>
-        </div>
+        </>
     )
 
 }
