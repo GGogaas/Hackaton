88cb8ac9cfd3afe9bb73f5372f688a8f6fe2854b
JacobCod3r <jak.mar.sp7@gmail.com>
2024-04-18 14:37:27 +0200

    Rolling dice almost done
    Almost done, I think...

--- src/DiceRollDND.js
+++ src/DiceRollDND.js
@@ -12,49 +12,135 @@ const DC = document.body.querySelector(".DCNumber");
 const img = document.body.querySelector(".img");
 const bonus = document.body.querySelector(".bonusNumber");
 const number = document.body.querySelector(".rollsNumber");
-const displayResult = document.body.querySelector(".result");
+const displayResult = document.body.querySelector(".resultOutput");
+const sums = document.body.querySelector(".sum");
+const sumOutput = document.body.querySelector(".sumOutput");
+const warning = document.body.querySelector(".warnings");
+
+let check = true;
+
 
 function start(){
-    function maxRoll() {
-        for(let i = 0; i < dicesList.length; i++){
-            if(dicesList[i].checked){
-                return dicesList[i].value;
+
+    if(check){
+
+        check = false;
+
+        if(!sums.classList.contains("none")){
+            sums.classList.add("none");
+            sumOutput.classList.add("none");
+        }
+
+        function maxRoll() {
+            for(let i = 0; i < dicesList.length; i++){
+                if(dicesList[i].checked){
+                    return dicesList[i].value;
+                }
             }
         }
-    }
 
-    let result = 0;
-    
+        let result = [];
+        let sum = 0;
 
+        rollNumber = number.value;
+        if(rollNumber <= 1 || rollNumber == ""){
+            rollNumber = 1;
+        }
 
-    for(let i = 0; i < 30; i++){
-        setTimeout(function() {
+        warning.classList.add("none");
+        if(maxRoll() != null){
+            for(let i = 0; i < 30; i++){
+                setTimeout(function() {
+                    displayResult.innerHTML = "";
+                    for(let i = 0; i < parseInt(rollNumber); i++){
+                        result[i] = Math.ceil(Math.random() * maxRoll());
+                        displayResult.innerHTML += (result[i] + " ");
+                    }
+                
+                }, i * 100);
+            }
+            if(!(rollNumber == "" || parseInt(rollNumber) <= 1)){
+                if(parseInt(bonus.value) > 0){
+                    setTimeout(function() {
+                        displayResult.innerHTML = ""; 
+                        for(let i = 0; i < parseInt(rollNumber); i++){
+                            displayResult.innerHTML += (result[i] + " + bonus");
+                            if(!(result.length - 1 == i)){
+                                displayResult.innerHTML += ", ";
+                            }
+                        }
+                        }, 100 * 30);
+                    setTimeout(function() {
+                        displayResult.innerHTML = ""; 
+                        for(let i = 0; i < parseInt(rollNumber); i++){
+                            result[i] += parseInt(bonus.value);
+                            sum += result[i];
+                            console.log(sum);
+                            displayResult.innerHTML += result[i];
+                            if(!(result.length - 1 == i)){
+                                displayResult.innerHTML += ", ";
+                            }
+                        }
 
-            result = Math.ceil(Math.random() * maxRoll());
-            displayResult.innerHTML = result;
-            console.log("");
-        
-        }, i * 100);
-    }
-    if(parseInt(bonus.value) > 0){
-        setTimeout(function() {
-            displayResult.innerHTML += " + bonus";
-            }, 100 * 30);
-        setTimeout(function() {
-            displayResult.innerHTML = parseInt(result) + parseInt(bonus.value);
-            isDC(parseInt(result));
-            }, 100 * 30 + 2000);
-    }
-    else{
-    setTimeout(function() {
-        isDC(parseInt(result));
-        }, 100 * 30);
+                        sums.classList.remove("none");
+                        sumOutput.innerHTML = sum;
+                        sumOutput.classList.remove("none");
+                        
+                        check = true;
+
+                        }, 100 * 30 + 2000);
+                }
+                else {
+                    setTimeout(function() {
+                        displayResult.innerHTML = "";
+                        for(let i = 0; i < parseInt(rollNumber); i++){
+                            displayResult.innerHTML += result[i];
+                            sum += result[i];
+                            if(!(result.length - 1 == i)){
+                                displayResult.innerHTML += ", ";
+                            }
+                        }
+
+                        sums.classList.remove("none");
+                        sumOutput.innerHTML = sum;
+                        sumOutput.classList.remove("none");
+
+                        check = true;
+                        
+                        }, 100 * 30);
+                }
+            }
+            else if(parseInt(bonus.value) > 0){
+                setTimeout(function() {
+                    displayResult.innerHTML += " + bonus";
+                    }, 100 * 30);
+                setTimeout(function() {
+                    displayResult.innerHTML = parseInt(result) + parseInt(bonus.value);
+                    isDC(parseInt(result));
+                    check = true;
+                    }, 100 * 30 + 2000);
+            }
+            else{
+            setTimeout(function() {
+                isDC(parseInt(result));
+                check = true;
+                }, 100 * 30);
+            }
+        }
+        else {
+            warning.innerHTML = "Select dice!";
+            warning.classList.remove("none");
+            check = true;
+        }
+    } 
+    else {
+        warning.innerHTML = "Wait until roll ends!";
+        warning.classList.remove("none");
     }
 }
 
 function isDC(result){
-    console.log(result);
-    if((number.value == "" || parseInt(number.value) <= 1) && (DC.value != "" || parseInt(DC.value) <= 0)){
+    if((rollNumber == "" || parseInt(rollNumber) <= 1) && (DC.value != "" || parseInt(DC.value) <= 0)){
         let bonusValue = 0;
         if(!(bonus.value == "" || bonus.value < 0)){
             bonusValue = parseInt(bonus.value)
