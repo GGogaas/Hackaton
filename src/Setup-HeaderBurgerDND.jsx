f9fee6abb0d6ac6486f143b97532a17206273a27
GGogaas <woj3333@gmail.com>
2024-04-19 16:06:16 +0200

    Revert "Merge branch 'main' of https://github.com/GGogaas/Hackaton"
    This reverts commit b11cf9bab5b23788c8ca1c272074294e6b7948ed, reversing
    changes made to 9aa93b1b03186b6527fd5626463d961838fa8e35.

--- src/Setup-HeaderBurgerDND.jsx
+++ src/Setup-HeaderBurgerDND.jsx
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom/client'
 import HeaderBurger from './HeaderBurger'
 import './index.css'
 const list = [
-  {name: "DNDDices.html", target: "_self", display: "Dices"},
-  {name: "DNDPCC.html", target: "_self", display: "Character Creation"},
-  {name: "DNDCombat.html", target: "_self", display: "Combat"},
-  {name: "DNDStatBlocks.html", target: "_self", display: "StatBlocks"},
-  {name: "DNDBooks.html", target: "_self", display: "Books"},
-  {name: "https://dnd.wizards.com/", target: "_blank", display: "D&D official website"},
-  {name: "https://www.dndbeyond.com/posts/1480-what-is-dungeons-dragons", target: "_blank", display: "What is D&D"}
+  {name: "DNDDices.html", display: "Dices"},
+  {name: "DNDPCC.html", display: "Character Creation"},
+  {name: "DNDCombat.html", display: "Combat"},
+  {name: "DNDStatBlocks.html", display: "StatBlocks"},
+  {name: "DNDBooks.html", display: "Books"},
+  {name: "https://dnd.wizards.com/", display: "DND official website"}
 ]
 ReactDOM.createRoot(document.getElementById('header-burger')).render(
   <React.StrictMode>
