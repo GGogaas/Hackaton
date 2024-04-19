3cb269b4942b32df5c803152a9bee2b5e96927cf
JacobCod3r <jak.mar.sp7@gmail.com>
2024-04-19 15:59:51 +0200

    Target _blank to outside websites

--- src/Setup-HeaderBurgerDND.jsx
+++ src/Setup-HeaderBurgerDND.jsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client'
 import HeaderBurger from './HeaderBurger'
 import './index.css'
 const list = [
-  {name: "DNDDices.html", display: "Dices"},
-  {name: "DNDPCC.html", display: "Character Creation"},
-  {name: "DNDCombat.html", display: "Combat"},
-  {name: "DNDStatBlocks.html", display: "StatBlocks"},
-  {name: "DNDBooks.html", display: "Books"},
-  {name: "https://dnd.wizards.com/", display: "DND official website"}
+  {name: "DNDDices.html", target: "_self", display: "Dices"},
+  {name: "DNDPCC.html", target: "_self", display: "Character Creation"},
+  {name: "DNDCombat.html", target: "_self", display: "Combat"},
+  {name: "DNDStatBlocks.html", target: "_self", display: "StatBlocks"},
+  {name: "DNDBooks.html", target: "_self", display: "Books"},
+  {name: "https://dnd.wizards.com/", target: "_blank", display: "DND official website"}
 ]
 ReactDOM.createRoot(document.getElementById('header-burger')).render(
   <React.StrictMode>
