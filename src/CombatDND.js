aaf555d72753dc2989ada8f5f96f2f6c54683689
JacobCod3r <jak.mar.sp7@gmail.com>
2024-04-20 18:06:57 +0200

    Adding to queue, counting turns and ending run

--- src/CombatDND.js
+++ src/CombatDND.js
@@ -1,8 +1,12 @@
-const CList = [];
+var CList = [];
+var CListSorted = [];
+var runMeterNumber = document.body.querySelector(".meter");
 
 if(localStorage.getItem("CList") != null){
     CList = localStorage.getItem("CList");
 }
+
+var number = CList.length;
 
 const popupBG = document.body.querySelector(".popupBG");
 const popup = document.body.querySelector(".popup");
@@ -10,6 +14,8 @@ const addButton = document.body.querySelector(".add");
 const startButton = document.body.querySelector(".start");
 const saveButton = document.body.querySelector(".save");
 const endButton = document.body.querySelector(".end");
+const endTurnButton = document.body.querySelector(".endTurn");
+const runMeter = document.body.querySelector(".runMeter");
 const warningBox = document.body.querySelector(".warnings");
 const queueDisplay = document.body.querySelector(".queueDisplay");
 
@@ -24,34 +30,50 @@ function displayAdd(){
 }
 
 function apply(){
-    if((nameD.value == "" || initiativeD.value == "" || hpD.value == "" || hpMaxD.value == "")) {
-        warningBox.classList.remove("none");
-        warningBox.innerHTML = "Fill up all informations!";
+    let canAdd = true;
+    if(CList.length > 0){
+        for(let i = 0; i < CList.length; i++){
+            if(CList[i].name == nameD.value){      
+                warningBox.classList.remove("none");
+                warningBox.innerHTML = "This name was already used!";
+                canAdd = false;
+            }
+        }
     }
-    else if(parseInt(hpD.value) > parseInt(hpMaxD.value)){
-        warningBox.classList.remove("none");
-        warningBox.innerHTML = "Current HP must be lower than max HP!";
-    }
-    else{
-        CList.push({name: nameD.value, initiative: parseInt(initiativeD.value), hp: parseInt(hpD.value), hpMax: parseInt(hpMaxD.value), note: noteD.value});
-        cancel();
-        displayCharacters();
-        warningBox.classList.add("none");
+    if(canAdd){
+        if((nameD.value == "" || initiativeD.value == "" || hpD.value == "" || hpMaxD.value == "")) {
+            warningBox.classList.remove("none");
+            warningBox.innerHTML = "Fill up all informations!";
+        }
+        else if(parseInt(hpD.value) > parseInt(hpMaxD.value)){
+            warningBox.classList.remove("none");
+            warningBox.innerHTML = "Current HP must be lower than max HP!";
+        }
+        else{
+            CList.push({name: nameD.value, initiative: parseInt(initiativeD.value), hp: parseInt(hpD.value), hpMax: parseInt(hpMaxD.value), note: noteD.value});
+            number = CList.length;
+            cancel();
+            displaySorted();
+            warningBox.classList.add("none");
+        }
     }
 }
 
+function displaySorted(){
+    CListSorted = CList.sort((a, b) => parseInt(b.initiative) - parseInt(a.initiative));
+    displayCharacters();
+}
+
 function displayCharacters(){
-    const CListSorted = CList.sort((a, b) => parseInt(b.initiative) - parseInt(a.initiative));
-    console.log(CListSorted);
     queueDisplay.innerHTML = "";
     for(let i = 0; i < CListSorted.length; i++){
         queueDisplay.innerHTML += 
             `
-            <div class="character">
-                <div>Name: ${CListSorted[i].name}</div><br>
-                <div>Initiative: ${CListSorted[i].initiative}</div><br>
-                <div>HP: ${CListSorted[i].hp}/${CListSorted[i].hpMax}</div>
-            </div>
+                <div class="character">
+                    <div>Name: ${CListSorted[i].name}</div><br>
+                    <div>Initiative: ${CListSorted[i].initiative}</div><br>
+                    <div>HP: ${CListSorted[i].hp}/${CListSorted[i].hpMax}</div>
+                </div>
             `;
     }
 }
@@ -61,6 +83,25 @@ function start(){
     startButton.classList.add("none");
     saveButton.classList.remove("none");
     endButton.classList.remove("none");
+    endTurnButton.classList.remove("none");
+    runMeter.classList.remove("none");
+}
+
+function endTurn(){
+    if(--number > 0){
+        let temp = CListSorted[0];
+        CListSorted.shift();
+        CListSorted.push(temp);
+        displayCharacters();
+    }
+    else {
+        number = CList.length;
+        let temp = CListSorted[0];
+        CListSorted.shift();
+        CListSorted.push(temp);
+        displayCharacters();
+        runMeterNumber.innerHTML++;
+    }
 }
 
 function cancel(){
@@ -71,4 +112,17 @@ function cancel(){
     noteD.value = "";
     popupBG.classList.add("none");
     warningBox.classList.add("none");
+}
+
+function end(){
+    localStorage.removeItem("CList");
+    addButton.classList.remove("none");
+    startButton.classList.remove("none");
+    saveButton.classList.add("none");
+    endButton.classList.add("none");
+    endTurnButton.classList.add("none");
+    runMeter.classList.add("none");
+    CList = [];
+    CListSorted = [];
+    displayCharacters();
 }
